aac085242033a423d9eb87de09e8435648330439
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-28 23:48:46 -0700

    Write download_project_html_pages

---
+++ Scraping/Projects/World Bank Projects/WB ICR Ratings/download_project_html_pages.py
@@ -0,0 +1,39 @@
+"""Using the project URLs downloaded from ./download_project_urls.py, downloads
+the html webpage for each project and saves them all to a folder.
+Warning: This will download ~5 GB of data, be prepared!
+"""
+
+import os
+import json
+import requests
+import progressbar  # install with `pip install progressbar2`
+
+URLS_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+URLS_FILE_PATH = os.path.join(
+    URLS_FILE_PATH, 'Data\Projects URLs\WB Projects Urls.txt')
+
+DATA_FOLDER_PATH = os.path.dirname(os.path.abspath(__file__))
+DATA_FOLDER_PATH = os.path.join(DATA_FOLDER_PATH, 'Data')
+DATA_FOLDER_PATH = os.path.join(DATA_FOLDER_PATH, 'Projects HTML')
+if not os.path.exists(DATA_FOLDER_PATH):
+    os.makedirs(DATA_FOLDER_PATH)
+
+
+def main():
+    # open downloaded urls
+    urls = []
+    with open(URLS_FILE_PATH, 'r') as urls_file:
+        urls = urls_file.readlines()
+    urls = [url.strip('\n') for url in urls]
+
+    for url in progressbar.progressbar(urls):
+        project_id = url.split('/')[-1]
+        data = requests.get(url)
+        with open(DATA_FOLDER_PATH + '\\' + project_id, 'wb') as output_file:
+            output_file.write(data.content)
+
+    print(f'Succesfully downloaded {len(urls)} files.')
+
+
+if __name__ == "__main__":
+    main()
