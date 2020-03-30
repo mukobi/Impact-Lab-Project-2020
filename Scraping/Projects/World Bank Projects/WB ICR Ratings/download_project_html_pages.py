e7a986fe862f86c9df1a9494d3fb11b0782a8294
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-29 17:42:40 -0700

    Update download_project_html_pages.py

--- Scraping/Projects/World Bank Projects/WB ICR Ratings/download_project_html_pages.py
+++ Scraping/Projects/World Bank Projects/WB ICR Ratings/download_project_html_pages.py
@@ -1,6 +1,6 @@
 """Using the project URLs downloaded from ./download_project_urls.py, downloads
 the html webpage for each project and saves them all to a folder.
-Warning: This will download ~5 GB of data, be prepared!
+Warning: This will download ~3 GB of data, be prepared!
 """
 
 import os
@@ -26,10 +26,14 @@ def main():
         urls = urls_file.readlines()
     urls = [url.strip('\n') for url in urls]
 
+    already_downloaded = os.listdir(DATA_FOLDER_PATH)
+
     for url in progressbar.progressbar(urls):
         project_id = url.split('/')[-1]
+        if project_id + '.html' in already_downloaded:
+            continue
         data = requests.get(url)
-        with open(DATA_FOLDER_PATH + '\\' + project_id, 'wb') as output_file:
+        with open(DATA_FOLDER_PATH + '\\' + project_id + '.html', 'wb') as output_file:
             output_file.write(data.content)
 
     print(f'Succesfully downloaded {len(urls)} files.')
